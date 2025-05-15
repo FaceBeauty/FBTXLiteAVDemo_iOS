9390f006cf91612340e4bb2701f52e180b7aa164
tao <2631124232@qq.com>
2025-05-15 17:21:38 +0800

    更新4.1.0-beta版本

--- MLVB-API-Example-OC/App/AppDelegate.m
+++ MLVB-API-Example-OC/App/AppDelegate.m
@@ -21,8 +21,25 @@
     [V2TXLivePremier setLicence:LICENSEURL key:LICENSEURLKEY];
     
     //todo --- facebeauty start1 ---
-    # error 需要FaceBeauty appid，与包名应用名绑定，请联系商务获取
-    [[FaceBeauty shareInstance] initFaceBeauty:@"YOUR_APP_ID" withDelegate:nil];
+
+    // 本地资源文件拷贝至沙盒路径
+    BOOL isResourceCopied = NO;
+    NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"FaceBeauty" ofType:@"bundle"];
+
+    NSArray *libraryPaths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+    if (libraryPaths.count > 0) {
+        NSString *libraryDirectory = [libraryPaths lastObject];
+        NSString *sandboxPath = [libraryDirectory stringByAppendingPathComponent:@"FaceBeauty"];
+        isResourceCopied = [[FaceBeauty shareInstance] copyResourceBundle:bundlePath toSandbox:sandboxPath];
+    }
+
+    NSString *version = [[FaceBeauty shareInstance] getVersion];
+    NSLog(@"当前FaceBeauty版本 %@", version ?: @"");
+
+    //    # error 需要FaceBeauty appid，与包名应用名绑定，请联系商务获取
+    if (isResourceCopied) {
+        [[FaceBeauty shareInstance] initFaceBeauty:@"YOUR_APP_ID" withDelegate:nil];
+    }
     //todo --- facebeauty end ---
     
     return YES;
